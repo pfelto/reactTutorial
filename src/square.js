3115d8e58c83c7d61c462ddfe26ff497f197f594
pfelto <paul.r.felton@gmail.com>
2021-10-14 15:01:20 -0400

    Completed the basic React Tutorial for tic-tac-toe. Incuded concept of lifting state and then some functions

--- src/square.js
+++ src/square.js
@@ -1,21 +1,46 @@
+import React from "react";
 import './index.css';
-
+/*
+Can try to change this later to pass it the square component. Now it is just a button event with target being button.square
+const handleClick = (e) => {
+    console.log(e);
+}
+*/
   //Using stateless functional components for Square to start
-  const Square = ({index}) => (
-    <button className = 'square'>
-        {index}
+  //need to state to track X or not so using class
+  //moved state to board so can make it a function again
+  //we can use arrow function like here or just make it a normal function
+  //function Square({value,click}){ return ( JSX ); }
+  const Square = ({value, click }) => (
+    <button 
+    className = 'square' 
+    onClick={click}>
+        {value}
     </button>
   );
 
 /*
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
       return (
-        <button className="square">
-          {this.props.index}
+        <button className="square" 
+        onClick={(e) => { 
+            this.setState({value: 'X'});
+            console.log(this);
+            //console.log(`Clicked ${this.props.index}`);
+            //handleClick(e);
+        }}>
+          {this.state.value}
         </button>
       );
     }
   }
-*/
+  */
+
 export default Square
